0a624feeb09c00566ae9b570b7e601d582d636f7
nori <>
2018-01-23 19:03:22 +0900

    modify migration

--- db/migrate/20171205095448_add_column_users.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :name, :string
-    add_column :users, :address1, :string
-    add_column :users, :address2, :string
-    add_column :users, :address3, :string
-    add_column :users, :nickname, :string
-  end
-end
