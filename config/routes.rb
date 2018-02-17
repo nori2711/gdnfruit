4350b20d7eb616f50de10a3894a4607c9ce30a60
nori <maruyama@maruyamahanwanoMacBook.local>
2018-02-17 15:17:31 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   root  'fruits#index'
   devise_for :users
+  resources :users
+  resources :trees
+
   resources :fruits do
     resources :harvests
   end
-  resources :users
+
   resources :harvests do
     resources :members
   end
+
   resources :harvests, only: :show do
     resources :reviews, only: [:new, :create]
   end
-  resources :trees
+
 end
